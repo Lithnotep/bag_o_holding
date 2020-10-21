2cf7e484a5c4870db24c4944b3a127a5ce79c489
Max Mitrani <mjmitrani168@gmail.com>
2020-10-21 14:48:05 -0600

    Update settings.py
    setup Postgres

--- bag_o_holding/settings.py
+++ bag_o_holding/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'bag_o_holding.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dnd',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
