dcb16614603a0238fa4fc1733fb4ffcfb1c86d53
Max Mitrani <mjmitrani168@gmail.com>
2020-10-21 14:08:15 -0600

    Update settings.py
    protected secret_key

--- bag_o_holding/settings.py
+++ bag_o_holding/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@alezce&!rlne0nn)vru4nddd759ad^-snii9$#3xks2dk5!6c'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
