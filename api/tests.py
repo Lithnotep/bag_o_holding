a033625b5a2cc8d1e1b8e927a2211de01b275951
Max Mitrani <mjmitrani168@gmail.com>
2020-10-23 15:52:57 -0600

    Update tests.py
    added user testing

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class UserModelTest(TestCase):
+    def test_user_create(self):
+        user = User.objects.create_user(username='Morra', email='test.com', password='password', first_name='Milly', last_name='Manny')
+        self.assertEqual(user.username, 'Morra')
+        self.assertEqual(user.email, 'test.com')
+        self.assertEqual(user.first_name, 'Milly')
+        self.assertEqual(user.last_name, 'Manny')
