094364291bdc1e8dcbee571f04097def60757261
Max Mitrani <mjmitrani168@gmail.com>
2020-10-26 15:16:00 -0600

    add UserSerializer
    add user serializer

--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+import json
+from django.http import JsonResponse, QueryDict
+from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from api.serializers import UserSerializer
 
-# Create your views here.
+
+
+class UserLogin(APIView):
+  parser_classes = [JSONParser]
+
+  def get(self, request):
+    user = User.objects.get(username=request.data[username])
+    
